72dd3174c2a1495e0177910fbbeec0e305f30bf3
AnnPrim <Primpraowann@gmail.com>
2024-12-04 14:09:51 -0600

    aligned profile and emojis

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Intro from "@/components/intro";
 export default function Home() {
   return (
     <main className="">
-        <Intro/>
+        <Intro />
     </main>
   )
 }
--- components/intro.tsx
+++ components/intro.tsx
@@ -9,7 +9,7 @@ export default function Intro() {
     return (
         <section>
             <div className="flex items-center justify-center">
-                <div>
+                <div className="relative">
                     <Image
                     src={profile}
                     alt="profile-image"
@@ -17,7 +17,12 @@ export default function Intro() {
                     height="192"
                     quality="95"
                     priority={true}
+                    className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+
                     />
+                    <span className="absolute text-4xl">
+                    🌟
+                    </span>
                 </div>
             </div>
         </section>
