1b2b9f4c7e036e1f29f9f79b62d17b2b9200711e
AnnPrim <Primpraowann@gmail.com>
2024-12-04 22:37:43 -0600

    profile emoji

--- components/intro.tsx
+++ components/intro.tsx
@@ -10,19 +10,19 @@ export default function Intro() {
         <section>
             <div className="flex items-center justify-center">
                 <div className="relative">
-                    <Image
-                    src={profile}
-                    alt="profile-image"
-                    width="192"
-                    height="192"
-                    quality="95"
-                    priority={true}
-                    className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+                    <div>
+                        <Image
+                        src={profile}
+                        alt="profile-image"
+                        width="192"
+                        height="192"
+                        quality="95"
+                        priority={true}
+                        className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
 
-                    />
-                    <span className="absolute text-4xl">
-                    🌟
-                    </span>
+                        />
+                    </div>
+                    <span className="absolute bottom-0 left-0 text-4xl"> 🌟 </span>
                 </div>
             </div>
         </section>
