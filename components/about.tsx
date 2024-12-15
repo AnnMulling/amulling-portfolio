e682d8db8ef9d5c68ca86d144ee27caf0bbae308
AnnPrim <Primpraowann@gmail.com>
2024-12-14 21:08:56 -0600

    divided section heading

--- components/about.tsx
+++ components/about.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function About() {
     return (
-        <section className="mb-24 max-w-[45rem] text-center leading-8">
+        <section className="mb-24 max-w-[45rem] text-center leading-8 sm:mb-40">
             <h2 className="text-3xl font-medium capitalize mb-8">About me</h2>
             <p className="mb-3">
                 I'm a creative and easy-going individual who loves tackling new challenges.
