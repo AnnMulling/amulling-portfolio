99e5f3008535568b96d0b6eff1c4708410872dbf
AnnPrim <Primpraowann@gmail.com>
2024-12-19 10:28:57 -0600

    working on projects

---
+++ components/projects.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SectionHeading from './section-heading'
+
+export default function Projects() {
+  return (
+   <section>
+        <SectionHeading>My projects
+
+        </SectionHeading>
+        <div>
+            
+        </div>
+   </section>
+  )
+}
