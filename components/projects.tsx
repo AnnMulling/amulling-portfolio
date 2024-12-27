59493c5722856aa68a96a121e7b777acdfe4155e
AnnPrim <Primpraowann@gmail.com>
2024-12-26 21:46:54 -0600

    group even and odd project image position

--- components/projects.tsx
+++ components/projects.tsx
@@ -22,7 +22,7 @@ export default function Projects() {
     )
 }
 
-type ProjectProps =  (typeof projectsData)[number]
+type ProjectProps = (typeof projectsData)[number]
 
 function Project({
     title,
@@ -31,22 +31,22 @@ function Project({
     imageUrl
 }: ProjectProps
 ) {
-    return <section className="bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0">
-        <div className="pt-4 pb-8 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full">
+    return <section className="group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 sm:grou-even:pl-8 hover:bg-gray-200 transition">
+        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
 
             <h3 className="text-2xl font-semibold">{title}</h3>
             <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
             <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
-                {tags.map((tag, index)=> (
+                {tags.map((tag, index) => (
                     <li className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full" key={index}>{tag}</li>
                 ))}
             </ul>
         </div>
         <Image
-        src={imageUrl}
-        alt="Projects"
-        quality={95}
-        className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl"
-         />
+            src={imageUrl}
+            alt="Projects"
+            quality={95}
+            className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:-right-[initial] group-even:-left-40"
+        />
     </section>
 }
