50848a706b3ff230cbd3294556db19604912c1c4
AnnPrim <Primpraowann@gmail.com>
2025-01-02 22:53:02 -0600

    even odd group padding

--- components/project.tsx
+++ components/project.tsx
@@ -31,10 +31,10 @@ export default function Project({
                 scale: scaleProgress,
                 opacity: opacityProgress,
             }}
-            className=" group mb-3 sm:mb-8 last:mb-0"
+            className="group mb-3 sm:mb-8 last:mb-0 even-pl-8"
         >
 
-            <section className="group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]  sm:grou-even:pl-8 hover:bg-gray-200 transition">
+            <section className=" bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]  hover:bg-gray-200 transition group-even:pl-8">
                 <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
 
                     <h3 className="text-2xl font-semibold">{title}</h3>
