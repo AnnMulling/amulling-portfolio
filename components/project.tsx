69d96f6daaa9e7032608cf76da5a19edac4e43dc
AnnPrim <Primpraowann@gmail.com>
2025-01-02 22:49:05 -0600

    editing scaling and opacity

--- components/project.tsx
+++ components/project.tsx
@@ -21,14 +21,15 @@ export default function Project({
         target: ref,
         offset: ["0 1", "1.33 1"],
     });
-    useTransform(scrollYProgress, [0, 1], [0.5, 1])
+    const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1])
+    const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1])
 
     return (
         <motion.div
             ref={ref}
             style={{
-                scale: scrollYProgress,
-                opacity: scrollYProgress,
+                scale: scaleProgress,
+                opacity: opacityProgress,
             }}
             className=" group mb-3 sm:mb-8 last:mb-0"
         >
