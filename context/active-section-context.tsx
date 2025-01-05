12dc10e4e5702f45074f5ed329b51e6f34afb277
AnnPrim <Primpraowann@gmail.com>
2025-01-04 22:15:11 -0600

    added context type

--- context/active-section-context.tsx
+++ context/active-section-context.tsx
@@ -1,5 +1,5 @@
 import React, { useState, createContext } from 'react';
-import { links }  from "@/lib/data";
+import { links } from "@/lib/data";
 
 
 type SectionName = typeof links[number]["name"];
@@ -8,12 +8,23 @@ type ActiveSectionContextProvider = {
     children: React.ReactNode;
 };
 
-const ActiveSectionContext = createContext(null);
+type ActiveSectionContextType = {
+    activeSection: SectionName;
+    setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>
+}
+
+const ActiveSectionContext = createContext<ActiveSectionContextType | null >(null);
 
 export default function ActiveSectionContextProvider({
     children,
 }: ActiveSectionContextProvider) {
-      const [activeSection, setActiveSection] = useState<SectionName>("Home");
+    const [activeSection, setActiveSection] = useState<SectionName>("Home");
 
-  return <ActiveSectionContext.Provider>{children}</ActiveSectionContext.Provider>
+    return (
+        <ActiveSectionContext.Provider
+            value={{
+                activeSection,
+                setActiveSection
+            }}>{children}</ActiveSectionContext.Provider>
+    )
 }
