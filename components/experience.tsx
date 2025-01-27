9b3c1a7054456df1ad3e098c5a778d3f9df02304
AnnPrim <Primpraowann@gmail.com>
2025-01-27 10:45:15 -0600

    completed expereience portion

--- components/experience.tsx
+++ components/experience.tsx
@@ -13,14 +13,14 @@ export default function Experience() {
   return (
     <section id="experience">
         <SectionHeading>My experience</SectionHeading>
-        <VerticalTimeline>
+        <VerticalTimeline lineColor="">
                 {experiencesData.map((item, index) => (
                         <React.Fragment key={index}>
                             <VerticalTimelineElement
                                 contentStyle={{
                                     background: "#f3f4f6",
                                     boxShadow: "none",
-                                    border: "1px solid rgba(0, 0, 0, 0.5)",
+                                    border: "1px solid rgba(0, 0, 0, 0.05)",
                                     textAlign: "left",
                                     padding: "1.3rem 2rem"
                                 }}
@@ -34,9 +34,9 @@ export default function Experience() {
                                     fontSize: "1.5rem"
                                 }}
                             >
-                                <h3>{item?.title}</h3>
-                                <p>{item?.location}</p>
-                                <p>{item?.description}</p>
+                                <h3 className="font-semibold captitalize">{item?.title}</h3>
+                                <p className="font-normal !mt-0">{item?.location}</p>
+                                <p className="!mt-1 !font-normal text-gray-700">{item?.description}</p>
                             </VerticalTimelineElement>
                         </React.Fragment>
                 ))}
