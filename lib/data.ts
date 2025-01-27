11294a2dce1125f9ca71f42c44880dae06117397
AnnPrim <Primpraowann@gmail.com>
2025-01-27 10:25:43 -0600

    new data expereince is added

--- lib/data.ts
+++ lib/data.ts
@@ -42,28 +42,29 @@ export const links = [
 export const experiencesData = [
   {
     title: "Graduated bootcamp",
-    location: "Miami, FL",
+    location: "App Academy",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      "Completed a six-month software engineering bootcamp covering Python, JavaScript, SQL, React, HTML, CSS, OOP, data structures, algorithms, TDD, and programming best practices.",
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: "2023",
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: "Graduated bootcamp",
+    location: "Generation USA",
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+      "Completed an 18-week Jr. Cloud Practitioner program with 300+ hours of training in cloud infrastructure deployment, configuration, and management. Gained basic knowledge of Linux, Python, AWS, and DevOps for cloud-based IT operations.",
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
+    date: "2021",
   },
   {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
+    title: "Bachelor's degree",
+    location: "Payap University",
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
-    date: "2021 - present",
+     "Earned a bachelor's degree in English from Thailand, focusing on language, communication, literature, and cultural studies.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2018",
   },
+,
 ] as const;
 
 export const projectsData = [
