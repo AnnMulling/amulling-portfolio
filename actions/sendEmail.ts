55302ac25bd125b61bc735fbfe251cf307d14d2d
AnnPrim <Primpraowann@gmail.com>
2025-02-07 16:13:26 -0600

    finished email error handling

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,32 +1,40 @@
 "use server";
 
 import { Resend } from "resend";
-import { validatateString } from "@/lib/utils";
+import { validatateString, getErrorMessage } from "@/lib/utils";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+
+
 //server side validation
 export const sendEmail = async (formData: FormData) => {
-  const senderEmail = formData.get("email");
+  const senderEmail = formData.get("senderEmail");
   const message = formData.get("message");
 
   if (!validatateString(sendEmail, 500)) {
     return {
-        error: "Invalid sender email",
+      error: "Invalid sender email",
     };
-  };
+  }
 
   if (!validatateString(message, 5000)) {
     return {
-        error: "Invalid message",
+      error: "Invalid message",
     };
-  };
+  }
 
-  resend.emails.send({
-    from: "onboarding@resend.dev",
-    to: "p.annmulling@gmail.com",
-    subject: "message from contact form",
-    reply_to: senderEmail as string,
-    text: message as string,
-  });
+  try {
+    await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: "p.annmulling@gmail.com",
+      subject: "message from contact form",
+      reply_to: senderEmail as string,
+      text: message as string,
+    });
+  } catch (error: any) {
+    return {
+      error: getErrorMessage(error),
+    };
+  }
 };
