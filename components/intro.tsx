bac77e5a1715db94394a7c5bd00350e582e4d67a
AnnPrim <Primpraowann@gmail.com>
2025-02-07 15:39:01 -0600

    successfully implemented email from resend

--- components/intro.tsx
+++ components/intro.tsx
@@ -102,18 +102,18 @@ export default function Intro() {
 
                 <a
                     href="/CV.pdf" download
-                    className=" group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition-all cursor-pointer border border-black/10"
+                    className=" group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition-all cursor-pointer border borderBlack"
                 >Download CV <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
                 </a>
 
                 <a
                     href="https://www.linkedin.com/in/ann-m-653708227/" target="_blank"
-                    className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 ctive:scale-105 transition-all cursor-pointer border border-black/10">
+                    className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 ctive:scale-105 transition-all cursor-pointer border borderBlack">
                     <BsLinkedin />
                 </a>
                 <a
                     href="https://github.com/AnnMulling" target="_blank"
-                    className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15]  hover:text-gray-950 active:scale-105 transition-all cursor-pointer border border-black/10">
+                    className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15]  hover:text-gray-950 active:scale-105 transition-all cursor-pointer border borderBlack">
                     <FaGithubSquare />
                 </a>
             </motion.div>
