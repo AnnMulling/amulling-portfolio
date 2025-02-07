9c7dd087eccc61e01c4a33e13e41b7c3d0a6d332
AnnPrim <Primpraowann@gmail.com>
2025-02-07 15:43:04 -0600

    pulling data from formData

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,20 +1,23 @@
 "use server";
 
-
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (formData: FormData) => {
+  const sender = formData.get("email");
+  const message = formData.get("message");
 
-    console.log("Running on server");
-    console.log(formData.get("senderEmail"));
-    console.log(formData.get("message"));
+  if (!message || typeof message !== "string") {
+    return {
+        error: "Invalid message"
+    };
+  }
 
-    resend.emails.send({
-        from: 'onboarding@resend.dev',
-        to: 'p.annmulling@gmail.com',
-        subject: 'message from contact form',
-        text: 'Hello Ann',
-    });
+  resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: "p.annmulling@gmail.com",
+    subject: "message from contact form",
+    text: message,
+  });
 };
