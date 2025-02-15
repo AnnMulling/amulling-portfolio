762fb09c554998c83da163f4d937bfec6fce6f7d
AnnPrim <Primpraowann@gmail.com>
2025-02-15 13:47:23 -0600

    styling expereince secton in dark theme

--- components/experience.tsx
+++ components/experience.tsx
@@ -29,7 +29,7 @@ export default function Experience() {
                                     padding: "1.3rem 2rem"
                                 }}
                                 contentArrowStyle={{
-                                    borderRight: "0.4rem solid #9ca3af",
+                                    borderRight: theme === 'light' ? "0.4rem solid #9ca3af" : "0.4rem solid rgba(255,255,255,0.5)"
                                 }}
                                 date={item?.date}
                                 icon={item?.icon}
